7ad8494bdb854b5f11bddb0f0bca3a1d0a38d997
Dhineshkumar M <dhineshkumar.m@cgvakindia.com>
2021-10-01 18:49:41 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using PdfDemo.Models;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace PdfDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IWebHostEnvironment _hostingEnvironment;
+
+        public HomeController(
+            IWebHostEnvironment environment)
+        {
+            _hostingEnvironment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public string OpenFile(string path)
+        {
+            path = Path.Combine(_hostingEnvironment.ContentRootPath, path);
+
+            byte[] b = System.IO.File.ReadAllBytes(path);
+            return Convert.ToBase64String(b);
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+.NET 5 
+
+This project demonstrates a sample implementation to open and view base64 PDF in new browser tab
+
+
+
